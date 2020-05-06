f42cba1ffc325c9f29b64a5df12e0f7fb687c399
Joao Oliveira <joao.oliveira@unparallel.pt>
2020-05-06 11:30:37 +0100

    added import of mongo key to truststore

--- start.sh
+++ start.sh
@@ -5,8 +5,10 @@ echo "Generating keystore"
 openssl pkcs12 -export -in ${SSL_CERT_PATH} -inkey ${SSL_KEY_PATH} -out /etc/.certs/tmp_keystore.p12 -passout pass:${KEYSTORE_PASS} -name notification
 echo "Remove old certificate from truststore"
 keytool -delete -alias notification -keystore $JAVA_HOME/lib/security/cacerts -noprompt -storepass changeit
+keytool -delete -alias mongo -keystore $JAVA_HOME/lib/security/cacerts -noprompt -storepass changeit
 echo "Importing to truststore"
 keytool -import -file ${SSL_CERT_PATH} -alias notification -keystore $JAVA_HOME/lib/security/cacerts -noprompt -storepass changeit
+keytool -import -file ${MONGO_PEM_KEY_PATH} -alias mongo -keystore $JAVA_HOME/lib/security/cacerts -noprompt -storepass changeit
 
 
 echo "Starting notification APP"
